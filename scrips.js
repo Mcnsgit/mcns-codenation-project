f10fcc61593b999ec653bda6446c8d9eee1f7a94
mcnsgit <cardigamiguel221@gmail.com>
2023-04-17 10:34:24 +0100

    updated code
    updated code

--- scrips.js
+++ scrips.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    const projects = document.querySelector('.projects');
+    const projects = document.querySelector('.projects-gallery');
     const response = await fetch('projects.html');
     const projectsHTML = await response.text();
     const parser = new DOMParser();
@@ -13,18 +13,23 @@ window.addEventListener('DOMContentLoaded', async () => {
         images.splice(randomIndex, 1);
     }
 
-    randomImages.forEach((img) => projects.appendChild(img));
+    randomImages.forEach((img) => {
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('project-image');
+        imageContainer.appendChild(img);
+        projects.appendChild(imageContainer);
+    });
 });
 
 $(document).ready(function () {
     $(".project-box").on("click", function () {
-        if ($(this).data("expanded")) {
+        if ($(this).hasClass("expanded")) {
             $(this).removeClass("expanded");
-            $(this).data("expanded", false);
+            $(this).find(".hidden-image").hide();
         } else {
-            $(".project-box.expanded").removeClass("expanded").data("expanded", false);
+            $(".project-box.expanded").removeClass("expanded").find(".hidden-image").hide();
             $(this).addClass("expanded");
-            $(this).data("expanded", true);
+            $(this).find(".hidden-image").show();
         }
     });
 });
